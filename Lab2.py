34efb50ffe8aa7220f7c5d1e9b276be69d0e9dab
arizonaseason <collegeemail2020s@gmail.com>
2025-02-14 13:37:49 -0800

    Update Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -1,10 +1,10 @@
 # GROUP 9: Duy Luu, Kristine Nguyen, Nitya Prasad
 # 2025SP-COMSC-077
 # Lab 2
+# This program aims to converts a floating-point number to its simplified floating-point representation or the IEEE 754 format discussed in the slides.
 
-# This program aims to
+import struct
 
-print ("Hello world!")
-
-def convertFloatingPoint(number):
-    print("something")
+def convertFloatingPoint(FP_num):
+    sign = 0 if FP_num >=0 else 1
+    FP_num = abs(FP_num)
