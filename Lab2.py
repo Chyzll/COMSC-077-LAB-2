3547886a4adf5430cfd2f92840c4a721d12e1e5f
Chyzll <butterb03@gmail.com>
2025-02-13 16:51:24 -0800

    Just starting the repository

---
+++ Lab2.py
@@ -0,0 +1,10 @@
+# GROUP 9: Duy Luu, Kristine Nguyen, Nitya Prasad
+# 2025SP-COMSC-077
+# Lab 2
+
+# This program aims to
+
+print ("Hello world!")
+
+def convertFloatingPoint(number):
+    print("something")
