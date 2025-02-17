7c9d7d7c59a60fd20e555ee7eec38fb8c34a33dc
arizonaseason <collegeemail2020s@gmail.com>
2025-02-16 21:04:57 -0800

    Update Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -4,26 +4,26 @@
 # This program aims to converts a floating-point number to its simplified floating-point representation or the IEEE 754 format discussed in the slides.
 
 
-def convertFloatingPoint(x):
+def convertFloatingPoint(f):
     #for the sign bit
     sign = 0 if x >=0 else 1
-    x = abs(x)
+    f = abs(f)
 
 
-#normallization
+#normalization
 exponent = 0
-while x >= 2.0:
-        x /= 2.0
+while f >= 2.0:
+        f /= 2.0
         exponent += 1
-while x < 1.0:
-        x *= 2.0
+while f < 1.0:
+        f *= 2.0
         exponent -= 1
 
 #With bias 15
 exponent_with_bias = exponent + 15 
 
  # The 8 bits (The significand/mantissa)
-significand = x - 1  #removes the 1
+significand = f - 1  #removes the 1
 mantissa_bits = []
 for _ in range(8):
         significand *= 2
